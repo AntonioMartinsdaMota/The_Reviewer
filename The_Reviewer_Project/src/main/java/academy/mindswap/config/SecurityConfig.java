386327b2bb817c414e87712230e6962754576557
João Souto <jpsoutost@gmail.com>
2022-03-30 16:55:48 +0100

    Corrected security config, chaged POM

--- The_Reviewer_Project/src/main/java/academy/mindswap/config/SecurityConfig.java
+++ The_Reviewer_Project/src/main/java/academy/mindswap/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package academy.mindswap.config;
 
-
-import academy.mindswap.lms.security.CookieFilter;
-import academy.mindswap.lms.security.MyAuthProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
