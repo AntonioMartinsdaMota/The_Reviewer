3be280a86449b385865e25c01e94da861b1aa631
João Souto <jpsoutost@gmail.com>
2022-04-03 10:49:21 +0100

    migrate refresh token to token service

--- The_Reviewer_Project/src/main/java/academy/mindswap/controllers/UserController.java
+++ The_Reviewer_Project/src/main/java/academy/mindswap/controllers/UserController.java
@@ -6,6 +6,7 @@ import academy.mindswap.commands.UserDto;
 import academy.mindswap.exceptions.notFoundExceptions.UserNotFoundException;
 import academy.mindswap.persistence.models.Role;
 import academy.mindswap.persistence.models.User;
+import academy.mindswap.services.TokenService;
 import academy.mindswap.services.UserService;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
@@ -36,6 +37,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TokenService tokenService;
 
     /**
      * Getters
@@ -105,35 +109,7 @@ public class UserController {
 
     @GetMapping("/token/refresh")//NOT AUTH
     public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String authorizationHeader = request.getHeader(AUTHORIZATION);
-
-        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
-            String refresh_token = authorizationHeader.substring("Bearer ".length());
-            Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-            JWTVerifier verifier = JWT.require(algorithm).build();
-            DecodedJWT decodedJWT = verifier.verify(refresh_token);
-            String email = decodedJWT.getSubject();
-
-            Optional<User> userOpt = userService.getUserByEmail(email);
-            if(userOpt.isEmpty()){
-                throw new UserNotFoundException(email);
-            }
-            User user = userOpt.get();
-
-            String access_token = JWT.create()
-                    .withSubject(user.getEmail())
-                    .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
-                    .withIssuer(request.getRequestURL().toString())
-                    .withClaim("roles", user.getRoles().stream().map(Role::getRole).collect(Collectors.toList()))
-                    .sign(algorithm);
-
-
-            Map<String, String> tokens = new HashMap<>();
-            tokens.put("access_token", access_token);
-            tokens.put("refresh_token", refresh_token);
-            response.setContentType(APPLICATION_JSON_VALUE);
-            new ObjectMapper().writeValue(response.getOutputStream(), tokens);
-        }
+        tokenService.refreshToken(request, response);
     }
 
 }
